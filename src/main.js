d6b2c44bfd1ec927b2efb53d82ee90c0e5087965
stopinder <chrysalisifs@robormiston.com>
2026-08-26 16:26:47 +0200

    feat(analytics): track consented public navigation

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import AuthGate from './AuthGate.vue'
 import router from './router'
 import './main.css'
+import { capturePublicPageView, getAnalyticsConsent, installPublicCtaTracking } from './lib/analytics.js'
 
 const app = createApp(AuthGate)
 app.use(router)
 
 router.isReady().then(() => {
   app.mount('#app')
+  installPublicCtaTracking()
+
+  if (getAnalyticsConsent() === true) capturePublicPageView(router.currentRoute.value)
+  router.afterEach((to) => {
+    if (getAnalyticsConsent() === true) capturePublicPageView(to)
+  })
 })
