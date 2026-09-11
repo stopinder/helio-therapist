e4322f1d9a22ade19407dcf9e669887dcdb66b11
stopinder <chrysalisifs@robormiston.com>
2026-09-11 08:58:59 +0200

    test(zoom): align transcript inbox icon

--- test/zoom-navigation-ui.test.js
+++ test/zoom-navigation-ui.test.js
@@ -33,7 +33,7 @@ test('Transcripts view supports direct transcript opening via query param', asyn
 
 test('AppSidebar contains Transcript Inbox link', async () => {
   const sidebar = await readFile(new URL('../src/components/shell/AppSidebar.vue', import.meta.url), 'utf8')
-  assert.match(sidebar, /\{ name: 'Transcript Inbox', path: '\/transcripts', icon: FileText, iconTone: 'icon-surface-reflection' \}/)
+  assert.match(sidebar, /\{ name: 'Transcript Inbox', path: '\/transcripts', icon: Inbox, iconTone: 'icon-surface-reflection' \}/)
 })
 
 test('Transcript Inbox supports Add New Client', async () => {
