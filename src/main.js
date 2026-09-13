063f03ec412f0b430a6cd3becbe98124b02a8068
Robert Ormiston <emdrifs@robormiston.com>
2026-09-13 14:36:32 +0200

    feat(analytics): add vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+import { inject } from '@vercel/analytics'
 import AuthGate from './AuthGate.vue'
 import router from './router'
 import './main.css'
+
+injectSpeedInsights()
+inject()
 
 const app = createApp(AuthGate)
 app.use(router)
 
 router.isReady().then(() => {
-  app.mount('#app')
+    app.mount('#app')
 })
